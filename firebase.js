12b44dc94c57f2309bf400bc9a6e05424ad99646
Bl4ck_Schnitzel <104429747+NewGamez@users.noreply.github.com>
2026-06-01 23:52:48 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDWn3eOlZGlXjzwKC0IK2SDjrsVhg1BtbI",
+  authDomain: "jugendheim-14aa9.firebaseapp.com",
+  projectId: "jugendheim-14aa9",
+  storageBucket: "jugendheim-14aa9.firebasestorage.app",
+  messagingSenderId: "388230097776",
+  appId: "1:388230097776:web:b2325e7435ef5f85c8fdb5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
