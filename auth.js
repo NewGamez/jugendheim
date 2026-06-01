a36f25a6279ee57c8d3427b2fd3e5d78bb47c6be
Bl4ck_Schnitzel <104429747+NewGamez@users.noreply.github.com>
2026-06-01 23:53:21 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,23 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+window.login = async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    window.location.href = "dashboard.html";
+
+  } catch(error) {
+    alert(error.message);
+  }
+};
