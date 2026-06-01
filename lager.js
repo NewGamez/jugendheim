3b2e395b1fcec6352c34703da22cebd8c353557f
Bl4ck_Schnitzel <104429747+NewGamez@users.noreply.github.com>
2026-06-01 23:56:35 +0200

    Update lager.js

--- lager.js
+++ lager.js
@@ -1,1 +1,56 @@
+import { db } from "./firebase.js";
 
+import {
+ collection,
+ addDoc,
+ getDocs
+}
+from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+window.speichern = async () => {
+
+  await addDoc(
+    collection(db, "lager"),
+    {
+      artikel:
+      document.getElementById("artikel").value,
+
+      anzahl:
+      Number(
+        document.getElementById("anzahl").value
+      )
+    }
+  );
+
+  laden();
+};
+
+async function laden() {
+
+  const liste =
+  document.getElementById("liste");
+
+  liste.innerHTML = "";
+
+  const daten =
+  await getDocs(
+    collection(db, "lager")
+  );
+
+  daten.forEach((doc) => {
+
+    const item = doc.data();
+
+    liste.innerHTML += `
+      <p>
+      ${item.artikel}
+      -
+      ${item.anzahl}
+      Stück
+      </p>
+    `;
+  });
+}
+
+laden();
